1a122a6a2cffb27ae503b5de6880fc37ea286a08
hakkd <ehakki@alumni.ubc.ca>
2023-06-21 23:13:30 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,67 @@
+//jshint esversion:6
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import ejs from "ejs";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import encrypt from "mongoose-encryption";
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username})
+        .then((foundUser) => {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                }
+            }
+        })
+        .catch((err) => {console.log(err)});
+
+});
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password,
+    });
+
+    newUser.save()
+        .then(() => {res.render("secrets")})
+        .catch(err => {console.log(err)});
+});
+
+
+app.listen(3000, () => {console.log("listening on port 3000");});
