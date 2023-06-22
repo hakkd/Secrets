3090e3f10f04c985b193a50462d84c33599e2a99
hakkd <ehakki@alumni.ubc.ca>
2023-06-21 23:39:19 -0700

    changed encryption to hashing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import express from "express";
 import ejs from "ejs";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import encrypt from "mongoose-encryption";
+import md5 from "md5";
 
 const app = express();
 
@@ -19,8 +19,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -55,7 +53,7 @@ app.post("/login", (req, res) => {
 app.post("/register", (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password,
+        password: md5(req.body.password),
     });
 
     newUser.save()
